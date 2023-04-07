d2eddb2abfde8c250f84ad9dd291fd660677d02c
Ziyuan Han <hanziyuan08@outlook.com>
2023-04-07 09:38:09 +0800

    Format files

--- index.js
+++ index.js
@@ -17,74 +17,74 @@ const START_FLAG = "<!--START_SECTION:blog-->";
 const END_FLAG = "<!--END_SECTION:blog-->";
 
 async function run () {
-    const octokit = github.getOctokit(GITHUB_TOKEN);
-    const issuesInfo = await octokit.rest.issues.listForRepo({
-        owner: OWNER ? OWNER : github.context.repo.owner,
-        repo: REPO ? REPO : github.context.repo.repo,
-        sort: 'updated-desc',
-        per_page: NUMBER,
-    });
-    core.debug(`Got issues: ${JSON.stringify(issuesInfo)}`);
-    const issues = issuesInfo.data;
-    if (issues.length === 0) {
-        console.log(`Get no issues, exit`);
-        return;
-    }
+  const octokit = github.getOctokit(GITHUB_TOKEN);
+  const issuesInfo = await octokit.rest.issues.listForRepo({
+    owner: OWNER ? OWNER : github.context.repo.owner,
+    repo: REPO ? REPO : github.context.repo.repo,
+    sort: 'updated-desc',
+    per_page: NUMBER,
+  });
+  core.debug(`Got issues: ${JSON.stringify(issuesInfo)}`);
+  const issues = issuesInfo.data;
+  if (issues.length === 0) {
+    console.log(`Get no issues, exit`);
+    return;
+  }
 
-    const readmeContent = fs.readFileSync("./README.md", "utf-8").split("\n");
-    let {startIdx, endIdx} = findIndex(readmeContent);
-    const oldReadmeContent = [...readmeContent];
-    // Clear old activities between START_FLAG and END_FLAG
-    readmeContent.splice(startIdx + 1, endIdx - startIdx - 1);
+  const readmeContent = fs.readFileSync("./README.md", "utf-8").split("\n");
+  let { startIdx, endIdx } = findIndex(readmeContent);
+  const oldReadmeContent = [...readmeContent];
+  // Clear old activities between START_FLAG and END_FLAG
+  readmeContent.splice(startIdx + 1, endIdx - startIdx - 1);
 
-    startIdx++;
-    const preArr = preFormatIssue(issues);
-    readmeContent.splice(startIdx, 0, preArr.join('\n'));
-    issues.forEach((issue, idx) => readmeContent.splice(startIdx + idx + preArr.length - 1, 0, formatIssue(idx, issue)));
-    if (oldReadmeContent.join("\n") === readmeContent.join("\n")) {
-        console.log('No updated.');
-        return;
-    }
+  startIdx++;
+  const preArr = preFormatIssue(issues);
+  readmeContent.splice(startIdx, 0, preArr.join('\n'));
+  issues.forEach((issue, idx) => readmeContent.splice(startIdx + idx + preArr.length - 1, 0, formatIssue(idx, issue)));
+  if (oldReadmeContent.join("\n") === readmeContent.join("\n")) {
+    console.log('No updated.');
+    return;
+  }
 
-    fs.writeFileSync("./README.md", readmeContent.join("\n"));
-    await commitFile();
+  fs.writeFileSync("./README.md", readmeContent.join("\n"));
+  await commitFile();
 }
 
-function formatIssue(idx, issue) {
-    const updateTime = `${dayjs(issue.updated_at).format('YYYY-MM-DD')}`;
-    const title = `[${issue.title}](${issue.html_url})`;
-    const summary = issue.body ? issue.body.trim().replaceAll('\r\n', ' ').substring(0, parseInt(SUMMARY_LENGTH)) : '';
-    return `| ${updateTime} | ${title} | ${summary} |`;
+function formatIssue (idx, issue) {
+  const updateTime = `${dayjs(issue.updated_at).format('YYYY-MM-DD')}`;
+  const title = `[${issue.title}](${issue.html_url})`;
+  const summary = issue.body ? issue.body.trim().replaceAll('\r\n', ' ').substring(0, parseInt(SUMMARY_LENGTH)) : '';
+  return `| ${updateTime} | ${title} | ${summary} |`;
 }
 
-function preFormatIssue(issues) {
-    return ['| UpdateTime | Title | Summary |', '| ------ | ------ | ------ |'];
+function preFormatIssue (issues) {
+  return ['| UpdateTime | Title | Summary |', '| ------ | ------ | ------ |'];
 }
 
-function findIndex(readmeContent) {
-    let startIdx = readmeContent.findIndex(content => content.trim() === START_FLAG);
-    const endIdx = readmeContent.findIndex(content => content.trim() === END_FLAG);
-    if (startIdx === -1 || endIdx === -1) {
-        throw new Error(`Couldn't find both ${START_FLAG} and ${END_FLAG} comment. Exiting!`);
-    }
-    return {startIdx, endIdx};
+function findIndex (readmeContent) {
+  let startIdx = readmeContent.findIndex(content => content.trim() === START_FLAG);
+  const endIdx = readmeContent.findIndex(content => content.trim() === END_FLAG);
+  if (startIdx === -1 || endIdx === -1) {
+    throw new Error(`Couldn't find both ${START_FLAG} and ${END_FLAG} comment. Exiting!`);
+  }
+  return { startIdx, endIdx };
 }
 
 const commitFile = async () => {
-    if (GITHUB_NAME && GITHUB_EMAIL) {
-        await exec.exec("git", ["config", "--global", "user.email", GITHUB_EMAIL]);
-        await exec.exec("git", ["config", "--global", "user.name", GITHUB_NAME]);
-    } else {
-        await exec.exec("git", ["config", "--global", "user.email", "41898282+github-actions[bot]@users.noreply.github.com"]);
-        await exec.exec("git", ["config", "--global", "user.name", "readme-bot"]);
-    }
-    await exec.exec("git", ["add", "README.md"]);
-    await exec.exec("git", ["commit", "-m", COMMIT_MSG]);
-    await exec.exec("git", ["push"]);
+  if (GITHUB_NAME && GITHUB_EMAIL) {
+    await exec.exec("git", ["config", "--global", "user.email", GITHUB_EMAIL]);
+    await exec.exec("git", ["config", "--global", "user.name", GITHUB_NAME]);
+  } else {
+    await exec.exec("git", ["config", "--global", "user.email", "41898282+github-actions[bot]@users.noreply.github.com"]);
+    await exec.exec("git", ["config", "--global", "user.name", "readme-bot"]);
+  }
+  await exec.exec("git", ["add", "README.md"]);
+  await exec.exec("git", ["commit", "-m", COMMIT_MSG]);
+  await exec.exec("git", ["push"]);
 };
 
 try {
-    run();
+  run();
 } catch (error) {
-    core.setFailed(error.message);
+  core.setFailed(error.message);
 }
